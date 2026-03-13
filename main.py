518a4ae7a31e2b7d2215978ad43bc0aabe556b67
irtiza-radif <sadmanstudent0@gmail.com>
2026-03-13 14:59:50 +0600

    ADD new file

---
+++ main.py
@@ -0,0 +1,7 @@
+# Simple Python program
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hello, {name}!")
+print(f"In 5 years, you will be {age + 5} years old.")
